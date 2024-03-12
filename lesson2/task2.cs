0c1b2dbc29882fbebd1771981b64b9acc35bb580
Gulcohra18 <gulcohrsultanov@gmail.com>
2024-03-12 23:16:23 +0400

    lesson4

--- lesson2/task2.cs
+++ lesson2/task2.cs
@@ -1,10 +1,1 @@
-﻿int[] numbers = { 25, 45, 85, 68, 78 };
-min=int.MinValue;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (min > numbers[i])
-    {
-        min = numbers[i];
-    }
-}
-Console.WriteLine(min);
+﻿
